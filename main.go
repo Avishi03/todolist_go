a0df50b85e6243a2b8ee6c9aef8d7a9b510b9440
Avishi Agrawal <168264534+Avishi03@users.noreply.github.com>
2026-03-30 19:49:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
-var shortGolang = "Watch Go crash course"
-var fullGolang = "Watch Nana's Golang Full Course"
-var rewardDessert = "Reward myself with a donut"
-var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+var taskItems = []string{} // initially empty
 
 func main() {
-  http.HandleFunc("/", helloUser)
-  http.HandleFunc("/show-tasks", showTasks)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
+	http.HandleFunc("/add-task", addTask)
+
+	http.ListenAndServe(":8080", nil)
 }
 
 func showTasks(writer http.ResponseWriter, request *http.Request) {
-  for _, task := range taskItems {
-    fmt.Fprintln(writer, task)
-  }
+	if len(taskItems) == 0 {
+		fmt.Fprintln(writer, "No tasks yet. Add one!")
+		return
+	}
+
+	for _, task := range taskItems {
+		fmt.Fprintln(writer, task)
+	}
 }
 
 func helloUser(writer http.ResponseWriter, request *http.Request) {
-  var greeting = "Hello user. Welcome to our Todolist App!"
-  fmt.Fprintln(writer, greeting)
+	html := `
+	<h1>Welcome to ToDo App</h1>
+	<form action="/add-task" method="POST">
+		<input type="text" name="task" placeholder="Enter new task" required>
+		<button type="submit">Add Task</button>
+	</form>
+	<br>
+	<a href="/show-tasks">View Tasks</a>
+	`
+	fmt.Fprintln(writer, html)
+}
+
+func addTask(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodPost {
+		task := request.FormValue("task")
+		if task != "" {
+			taskItems = append(taskItems, task)
+		}
+	}
+	http.Redirect(writer, request, "/show-tasks", http.StatusSeeOther)
 }
